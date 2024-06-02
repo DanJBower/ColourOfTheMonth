cdcc20e613314ca4adf3d5eed25b076587c7dfb4
DanJBower <bowerdanielj@gmail.com>
2024-06-02 22:42:26 +0100

    Temporarily disabled light mode preview

--- ColourOfTheMonth/app/src/main/java/com/danjbower/colourofthemonth/MainActivity.kt
+++ ColourOfTheMonth/app/src/main/java/com/danjbower/colourofthemonth/MainActivity.kt
@@ -27,10 +27,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(
+/*@Preview(
     name = "Light Mode",
     uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL
-)
+)*/
 @Preview(
     name = "Dark Mode",
     uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
