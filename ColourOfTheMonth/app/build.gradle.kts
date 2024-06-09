b267d15f75f8fd35f70f7d22930dc5542a4c6fc7
DanJBower <bowerdanielj@gmail.com>
2024-06-09 12:17:07 +0100

    Changed to SDK 26 to just use adaptive icon

--- ColourOfTheMonth/app/build.gradle.kts
+++ ColourOfTheMonth/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.danjbower.colourofthemonth"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "0.0.1"
